c9bae76b27ef938b6f8baed3e01cb04af899146a
Stuart Wilcox <stuartwilcox@microsoft.com>
2022-03-23 16:27:38 -0700

    Complete Module 7&8

---
+++ src/8. Flow of Control/destructuring.rs
@@ -0,0 +1,7 @@
+// A match block can destructure items in a variety of ways.
+
+// Destructuring Tuples
+// Destructuring Arrays and Slices
+// Destructuring Enums
+// Destructuring Pointers
+// Destructuring Structures
---
+++ src/8. Flow of Control/for_loops.rs
@@ -0,0 +1,30 @@
+fn main_1() {
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}
+
+
+fn main() {
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            &"Bob" => (),
+            _ => println!("Hello {}", name),
+        }
+    }
+    
+    println!("names: {:?}", names);
+}
